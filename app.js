d888338f93793aeee6f4e3c43cccd92aebda6e84
Elijah Darkeh <elijahagbedam18@gmail.com>
2023-11-25 12:55:07 +0000

    changed background color of the page on button click

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-
-
-const button = document.querySelector("button")
-button.addEventListener('click', () => {
-  alert("Yay, I love puppies")
-})
+const button = document.querySelector("button");
+const body = document.querySelector("body");
+button.addEventListener("click", () => {
+  alert("Yay, I love puppies");
+  body.style.backgroundColor = "red";
+});
